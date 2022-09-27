17e42650d0c1f9a73779feb942fbc67d67b7432a
mikiyawv <mikiyawv@gmail.com>
2022-09-27 13:57:17 -0700

    Finished exercises 1, 2, and 3

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,7 @@
 const helloWorld = require('./helloWorld');
 
-describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
+describe('Hello World', () => {
+  test('says "Hello, World!"', () => {
     expect(helloWorld()).toEqual('Hello, World!');
   });
 });
--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let combinedWord = '';
+        for(let i = 0; i < num; i++) {
+            combinedWord += word;
+        }
+        return combinedWord; 
+}
 
 // Do not edit below this line
 module.exports = repeatString;
