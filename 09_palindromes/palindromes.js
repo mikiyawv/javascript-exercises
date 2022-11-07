5889aa5588aa0d300f55124fb250c17f9142687c
mikiyawv <mikiyawv@gmail.com>
2022-11-07 10:54:23 -0800

    Finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function(word) {
+    let processedWord = word.toLowerCase().replace(/[^a-z]/g, '');
+    let reverseWord = processedWord.split('').reverse().join('');
+    return processedWord === reverseWord;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
