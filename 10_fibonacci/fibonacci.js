3db62d290bd9ab289a0597f0b4432551277b3a34
mikiyawv <mikiyawv@gmail.com>
2022-11-08 14:03:18 -0800

    Fundamentals part 5 exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    let a = 0;
+    let b = 1;
+    let sum;
+    
+    let num = parseInt(count);
 
+    if (num === 1) return 1;
+    if (num < 1) return 'OOPS';
+
+    for (let i = 1; i < num; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((a, b) => {
+        let oldestAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        let currentAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        return oldestAge > currentAge ? a : b;
+    }
+)};
 
-};
+const getAge = function(birth, death) {
+    if (!death) death = new Date().getFullYear();
+
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
